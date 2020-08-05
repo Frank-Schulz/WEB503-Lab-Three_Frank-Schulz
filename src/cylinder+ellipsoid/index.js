ecb62b281443ed29752584524031b7ef85875e76
Frank Schulz <Frank-Schulz@live.nmit.ac.nz>
2020-08-06 11:29:06 +1200

    created cylinder and ellipsoid

--- src/cylinder+ellipsoid/index.js
+++ src/cylinder+ellipsoid/index.js
@@ -1,13 +1,39 @@
+
+// Variable init
+let h = 0.0;
+let test = 20;
+
 function setup() {
     // create a canvas
     createCanvas(500, 500, WEBGL);
+    // set frame rate
+    frameRate(60);
+    // Set angle mode to degrees
+    angleMode(DEGREES);
 }
 
 function draw() {
     // set background colour
     background("orange");
-    // move to center
-    translate(width/2, height/2);
+    // apply material
     normalMaterial();
-    cylinder(50, 50);
+
+    // define current height
+    h = test*sin(2*frameCount-5)+test;
+
+    // translate cylinder
+    translate(0, -h/2);
+    // create cylinder
+    cylinder(25, h);
+
+    
+    // move elipsoid
+    translate(0, -h/2-25);
+    // rotate ellipsoid
+    rotateY(frameCount*-5);
+    // rotate ellipsoid on to side
+    rotateX(90);
+    // create ellipsoid
+    ellipsoid(25, 50);
+
 }
